8b0a776a351ef8964a7bd0e6f358c1dbd753e12c
Chad Heise <chad@heise.org>
2026-05-15 23:07:42 -0700

    Try new vite port

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
+    port: 8080,
     proxy: {
-      '/api': 'http://localhost:8000',
-      '/ws': {
-        target: 'ws://localhost:8000',
+      "/api": "http://localhost:8000",
+      "/ws": {
+        target: "ws://localhost:8000",
         ws: true,
       },
     },
   },
-})
+});
