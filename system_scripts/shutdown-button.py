a109279fd1447e9726055b3d4557e9e9d6c117c2
Chad Heise <chad@heise.org>
2026-05-31 19:18:34 -0700

    Update shutdown service

--- system_scripts/shutdown-button.py
+++ system_scripts/shutdown-button.py
@@ -8,9 +8,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
-    GPIO.wait_for_edge(BUTTON_PIN, GPIO.FALLING)
-    time.sleep(0.1)  # debounce
-    if GPIO.input(BUTTON_PIN) == GPIO.LOW:
-        subprocess.run(['sudo', 'shutdown', '-h', 'now'])
+    while True:
+        GPIO.wait_for_edge(BUTTON_PIN, GPIO.FALLING)
+        time.sleep(0.1)  # debounce
+        if GPIO.input(BUTTON_PIN) == GPIO.LOW:
+            subprocess.run(['shutdown', '-h', 'now'])
 finally:
     GPIO.cleanup()
